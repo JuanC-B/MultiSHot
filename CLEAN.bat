827ea73e17591173e43a854019a8b1404c2fa32e
JaviD <javxdefez@gmail.com>
2024-10-03 14:28:16 +0200

    Primer Push Proyecto - Multiplayer Conectado

---
+++ CLEAN.bat
@@ -0,0 +1,12 @@
+rmdir /s /q Binaries
+rmdir /s /q Intermediate
+rmdir /s /q DerivedDataCache
+rmdir /s /q Build
+rmdir /s /q .vs
+del **.opensdf
+del **.sdf
+del **.sln
+del **.suo
+del **.xcodeproj
+
+pause
---
+++ Source/EOS_Server_VR_54/EOS_Server_VR_54.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EOS_Server_VR_54.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EOS_Server_VR_54, "EOS_Server_VR_54" );
